db7300f3284b0487223d452210008c5ea8ff3814
Ali Moustafa <103620928+alimoustafa2000@users.noreply.github.com>
2022-05-27 07:56:28 +0200

    Create 02.py

---
+++ Loop/02.py
@@ -0,0 +1,12 @@
+friends = ["Mohamed", "Shady", "ahmed", "eman", "Sherif", "ola"]
+
+
+a = 0
+friends == friends.sort()
+while friends[a] == friends[a].capitalize():
+    print(friends[a])
+    a += 1
+        
+    
+else:
+    print(f"Friends Printed And Ignored Names Count Is: {len(friends) - a}")
