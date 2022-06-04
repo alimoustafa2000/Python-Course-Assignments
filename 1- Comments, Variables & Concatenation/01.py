6bcc163e5db78f82e51559fd1800a52011be768f
Ali Moustafa <103620928+alimoustafa2000@users.noreply.github.com>
2022-06-04 09:52:43 +0200

    1st task

--- 1- Comments, Variables & Concatenation/01.py
+++ 1- Comments, Variables & Concatenation/01.py
@@ -1,1 +1,27 @@
+# ----------------------------------------
+# This is my first python file
+# I'm ali moustafa
+# I'm from Egypt
+# I started learning python today
+# ----------------------------------------
+
+name = "Ali"
+age = "22"
+country = "Egypt"
+
+
+print(type(name))
+print(type(age))
+print(type(country))
+
+print("=" * 40)
+
+print("Name:" + name)
+print("Age:" + age)
+print("Country:" + country)
+
+
+print("=" * 40)
+
+print("Hello, My Name Is" + " " + name + "," + " " + "Iam" + " " + age + " " + "Years Old" + " " + "And I'm from" + " " + country + ".")
 
